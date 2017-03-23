fe9bea0d4fce5fcb3646e26ad144a0bde3a110b1
lkdml <marianolopezsenes@gmail.com>
2017-03-23 17:53:46 -0300

    Implementando Listener

--- app/src/main/java/com/example/lkdml/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lkdml/myapplication/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.lkdml.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = (TextView) findViewById(R.id.texto1);
+        tv = (TextView) findViewById(R.id.texto1);
         tv.setText("hola Android");
+        Button btn = (Button) findViewById(R.id.btn1);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        this.tv.setText("Nos vemos.!");
     }
 }
